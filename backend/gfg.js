a8acb3efb3505209cc062caec12c6e9c23630c94
Nishant Saini <161240067+nishant21saini@users.noreply.github.com>
2025-02-27 19:03:06 +0530

    Update gfg.js

--- backend/gfg.js
+++ backend/gfg.js
@@ -19,8 +19,8 @@ geekrouter.get('/:username/solved', async (req, res) => {
     let browser;
     try {
         browser = await puppeteer.launch({
-            headless: true,
-               executablePath: '/opt/render/.cache/puppeteer/chrome/linux-133.0.6943.126/chrome-linux64/chrome', 
+            executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || '/usr/bin/google-chrome-stable', 
+               headless: "new",
             args: ['--no-sandbox', '--disable-setuid-sandbox'],
         });
         const page = await browser.newPage();
