f8587900c35d80a632ddcef91383de5a6cdbf31c
Nishant Saini <161240067+nishant21saini@users.noreply.github.com>
2025-02-27 17:44:14 +0530

    Update leetEvent.js

--- backend/eventTracker/leetEvent.js
+++ backend/eventTracker/leetEvent.js
@@ -11,7 +11,7 @@ leetEvent.get('/tracker' , async (req,res) => {
     console.log("fetching Event data ...");
 
     const browser = await puppeteer.launch({
-        executablePath: '/usr/bin/google-chrome',
+            executablePath: '/opt/render/.cache/puppeteer/chrome/linux-133.0.6943.126/chrome-linux64/chrome', 
         headless: true,
         args: [
             '--no-sandbox',
