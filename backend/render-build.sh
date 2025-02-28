e1c4cdf59aa9324658147d890baf818e9ec69204
Nishant Saini <161240067+nishant21saini@users.noreply.github.com>
2025-02-28 22:20:19 +0530

    Update render-build.sh

--- backend/render-build.sh
+++ backend/render-build.sh
@@ -8,9 +8,11 @@ npm install
 if [[ ! -d "$PUPPETEER_CACHE_DIR" ]]; then 
   echo "...Copying Puppeteer Cache from Build Cache" 
   cp -R "$XDG_CACHE_HOME/puppeteer/" "$PUPPETEER_CACHE_DIR"
-else 
+elif [[ "$(realpath "$PUPPETEER_CACHE_DIR")" != "$(realpath "$XDG_CACHE_HOME")" ]]; then
   echo "...Storing Puppeteer Cache in Build Cache" 
   cp -R "$PUPPETEER_CACHE_DIR" "$XDG_CACHE_HOME"
+else
+  echo "...Cache is already correct, skipping copy."
 fi
 
 # Install Chrome for Puppeteer
